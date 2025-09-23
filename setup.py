de233e94c32385e4266caf99f1deebbac5b521b0
Sharath M S <sharathsharu9155@gmail.com>
2025-09-23 10:15:27 -0500

    Created ML Components, Pipeline, logging and pipeline

---
+++ setup.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from setuptools import find_packages, setup
+
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_name: str) -> List[str]:
+    """
+    This function returns required packages to be installed
+    :param file_name: Name of the requirement list file
+    :return: List of packages needs to be installed
+    """
+    with open(file_name) as file_obj:
+        requirements = file_obj.readlines()
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+    return List[requirements]
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Sharath",
+    author_email="sharathsharu9155@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
---
+++ src/exception.py
@@ -0,0 +1,27 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    error_message = f'Error occured in Python script name "{exc_tb.tb_frame.f_code.co_filename}" in line number {exc_tb.tb_lineno} \nerror message: {str(error)}'
+
+    return error_message
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+
+    def __str__(self):
+        return self.error_message
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    try:
+        a=1/0
+    except Exception as e:
+        logger.info("Number cannot be divided by zero")
+        raise CustomException("Number cannot be divided by zero", sys)
